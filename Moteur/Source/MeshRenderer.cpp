928bb775ba018bedcbfa5ce5a5fda37122a55262
Titouan Le Floch <107429837+titouan-lef@users.noreply.github.com>
2023-10-30 16:27:56 +0100

    Début du meshrenderer

--- Moteur/Source/MeshRenderer.cpp
+++ Moteur/Source/MeshRenderer.cpp
@@ -1,2 +1,7 @@
 #include "Component.h"
 #include "MeshRenderer.h"
+
+MeshRenderer::MeshRenderer(ConstantBufferData* constBufferData)
+{
+	m_constBuffer = new ConstantBuffer(constBufferData);
+}
--- Moteur/Source/MeshRenderer.h
+++ Moteur/Source/MeshRenderer.h
@@ -1,13 +1,20 @@
 #pragma once
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "ConstantBuffer.h"
 
 class MeshRenderer : public Component
 {
 public:
-	MeshRenderer();
+	MeshRenderer(ConstantBufferData* constBufferData);
 	virtual ~MeshRenderer();
 
 
 private:
 	std::string m_shaderPath;
+
+//	VertexBuffer* m_vertexBuffer;
+//	IndexBuffer* m_indexBuffer;
+	ConstantBuffer* m_constBuffer;
 
 };
