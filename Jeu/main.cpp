529a549c80066072db89106e310e15856a0a929f
Titouan Le Floch <107429837+titouan-lef@users.noreply.github.com>
2023-11-06 16:24:10 +0100

    main initial

--- Jeu/main.cpp
+++ Jeu/main.cpp
@@ -6,19 +6,5 @@
 
 int main()
 {
-    try {
-
-
-        return App{}.Go();
-    }
-    catch (const EngineException& e) {
-        MessageBox(nullptr, MyException::convertCharArrayToLPCWSTR(e.what()), MyException::convertCharArrayToLPCWSTR(e.GetType()), MB_OK | MB_ICONEXCLAMATION);
-    }
-    catch (const std::exception& e) {
-        MessageBox(nullptr, MyException::convertCharArrayToLPCWSTR(e.what()), MyException::convertCharArrayToLPCWSTR("Standard Exception"), MB_OK | MB_ICONEXCLAMATION);
-    }
-    catch (...) {
-        MessageBox(nullptr, MyException::convertCharArrayToLPCWSTR("No Details Available"), MyException::convertCharArrayToLPCWSTR("Unknown Exception"), MB_OK | MB_ICONEXCLAMATION);
-    }
-    return -1;
+    return 0;
 }
